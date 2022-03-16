56fea4d0fba21a4fbf2d773825f13c65b6a0db5f
Mihran <xachatryan200104@gmail.com>
2022-03-16 19:08:33 +0400

    changes

--- src/store/Reducers/index.tsx
+++ src/store/Reducers/index.tsx
@@ -10,20 +10,40 @@ export default function reducer(state: IState, action: any) {
     //   state.choosenPizzas.push(action.payload.data);
     //   return state;
     case "CHOOSEN_PIZZAS":
-      // console.log(action.payload.data, "payload dataaa");
-
-      state.choosenPizzas.push(action.payload.data);
-      // return state;
-      return { ...state, totalCount: action.payload.data.totalCount };
+      return {
+        ...state,
+        choosenPizzas: [...state.choosenPizzas, action.payload.data],
+        totalCount: action.payload.data.totalCount,
+      };
 
     case "ADD_CHOOSEN_COUNT":
-      const choosenPizzas = state.choosenPizzas.map((item) => {
-        if (item.id === action.payload.id) {
-          item.count += 1;
-        }
-        return item;
-      });
-      return { ...state, choosenPizzas };
+      return {
+        ...state,
+        choosenPizzas: state.choosenPizzas.map((item) => {
+          if (item.id === action.payload.id) {
+            return {
+              ...item,
+              count: item.count + 1,
+            };
+          }
+          return item;
+        }),
+      };
+
+    case "REDUCE_COUNT":
+      return {
+        ...state,
+        choosenPizzas: state.choosenPizzas.map((item) => {
+          if (item.id === action.payload.id) {
+            return {
+              ...item,
+              count: item.count - 1,
+            };
+          }
+          return item;
+        }),
+      };
+
     default:
       return state;
   }
